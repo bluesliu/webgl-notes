ed1b3483c6651e044ed48827d8d2fae6187200cc
刘渊 <hiliuyuan@qq.com>
2020-08-01 00:53:22 +0800

    feat: 添加第2章

---
+++ chapter02/example/js/DrawRectangle.js
@@ -0,0 +1,6 @@
+function main() {
+    var canvas = document.getElementById("example");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = 'rgba(0,0,255,1)';
+    ctx.fillRect(120, 10, 150, 150);
+}
